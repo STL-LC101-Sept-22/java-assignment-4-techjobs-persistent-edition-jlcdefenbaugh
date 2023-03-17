063334486060082592d1cdcce9a8711e304d3cd8
Jami <jamidefenbaugh@gmail.com>
2023-03-16 20:47:10 -0500

    completed some work on tast 2, all but 3 tests passing

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
 
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 25, message = "Location must be between 3 and 25 characters")
+    private String location;
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {};
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
